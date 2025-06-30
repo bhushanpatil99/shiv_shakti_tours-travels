c7fa1d1fb7f999b9d520b54831d4696bf955eff9
bhushanpatil99 <bhushan3308@gmail.com>
2025-06-30 18:24:23 +0530

    Delete scripts/components/ui/button.jsx

--- scripts/components/ui/button.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Button = ({ children, className }) => (
-  <button className={`rounded-2xl shadow px-4 py-2 font-semibold ${className}`}>
-    {children}
-  </button>
-);
